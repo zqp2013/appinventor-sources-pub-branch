9479f4cc4039339223073b902f4d3d695a9e7af8
tsingfun <zqp2013@126.com>
2023-01-02 14:49:31 +0800

    恢复帮助下ai伴侣更新菜单

--- appinventor/appengine/src/com/google/appinventor/client/TopToolbar.java
+++ appinventor/appengine/src/com/google/appinventor/client/TopToolbar.java
@@ -392,8 +392,8 @@ public class TopToolbar extends Composite {
     }
     helpItems.add(new DropDownItem(WIDGET_NAME_COMPANIONINFO, MESSAGES.companionInformation(),
         new AboutCompanionAction()));
-    //helpItems.add(new DropDownItem(WIDGET_NAME_COMPANIONUPDATE, MESSAGES.companionUpdate(),
-    //    new CompanionUpdateAction()));
+    helpItems.add(new DropDownItem(WIDGET_NAME_COMPANIONUPDATE, MESSAGES.companionUpdate(),
+        new CompanionUpdateAction()));
     //helpItems.add(new DropDownItem(WIDGET_NAME_SHOWSPLASH, MESSAGES.showSplashMenuItem(),
     //    new ShowSplashAction()));
     refreshMenu(helpDropDown, helpItems);
